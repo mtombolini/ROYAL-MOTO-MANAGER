c97a56846af228efa43b49cda04573b50ef70823
mtombolini <mtombolini@uc.cl>
2024-01-04 18:16:10 -0300

    Arreglo ruta base

--- routes/home.py
+++ routes/home.py
@@ -6,9 +6,9 @@ from decorators.roles import requires_roles
 home_blueprint = Blueprint('home', __name__)
 
 # @requires_roles('desarrollador')
-# @home_blueprint.route('/')
-# def index():
-#     return redirect(url_for('auth.login'))
+@home_blueprint.route('/')
+def index():
+    return redirect(url_for('auth.login'))
 
 
 @home_blueprint.route('/home')
