35ffa661632b3f2c96bbbf9affa350f7a0f90f84
Franco Anfossi <franco.anfossi@uc.cl>
2024-01-11 18:01:55 -0300

    puntos y comas productos

--- models/productos.py
+++ models/productos.py
@@ -4,6 +4,10 @@ from databases.base import Base
 from databases.session import AppSession
 import pandas as pd
 
+def format_number(number):
+    return "${:,.2f}".format(number).replace(",", "X").replace(".", ",").replace("X", ".")
+
+
 from models.consumption import ConsumptionDetail
 from models.reception import ReceptionDetail
 from models.document import DocumentDetail
@@ -47,63 +51,6 @@ class Product(Base):
             try:
                 product = session.query(cls).filter(cls.variant_id == variant_id).first()  # Retrieve a single product
                 stock = product.stock.__dict__
-                
-                reception = [
-                    {
-                        key: value 
-                        for key, value in reception_detail.reception.__dict__.items() 
-                        if not key.startswith('_')
-                    }
-                for reception_detail in product.reception_details
-                ]
-
-                reception_details = [
-                    {
-                        key: value 
-                        for key, value in reception_detail.__dict__.items() 
-                        if not key.startswith('_')
-                    }
-                for reception_detail in product.reception_details
-                ]
-
-                consumptions = [
-                    {
-                        key: value 
-                        for key, value in consumption_detail.consumption.__dict__.items() 
-                        if not key.startswith('_')
-                    }
-                for consumption_detail in product.consumption_details
-                ]
-
-                consumption_details = [
-                    {
-                        key: value 
-                        for key, value in consumption_detail.__dict__.items() 
-                        if not key.startswith('_')
-                    }
-                for consumption_detail in product.consumption_details
-                ]
-
-                document_details = [
-                    {
-                        key: value 
-                        for key, value in document_detail.__dict__.items() 
-                        if not key.startswith('_')
-                    }
-                for document_detail in product.document_details
-                ]
-
-                documents = [document_detail.document for document_detail in product.document_details]
-
-                documentos = []
-                sales = []
-                sales_docs = []
-                for document in documents:
-                    if document.sales != []:
-                        documentos.append(document.__dict__)
-                        for sale in document.sales:
-                            sales_docs.append(sale.__dict__)
-                            sales.append(sale.sale.__dict__)
 
                 data = []
                 for reception_detail in product.reception_details:
@@ -120,7 +67,7 @@ class Product(Base):
                         "oficina": reception.office,
                         "nota": reception.note,
                         "cantidad": reception_detail.quantity,
-                        "costo_neto": reception_detail.net_cost
+                        "costo_neto": format_number(reception_detail.net_cost)
                     })
 
                 df = pd.DataFrame(data)
@@ -137,7 +84,7 @@ class Product(Base):
                         "oficina": consumption.office,
                         "nota": consumption.note,
                         "cantidad": consumption_detail.quantity,
-                        "costo_neto": consumption_detail.net_cost
+                        "costo_neto": format_number(consumption_detail.net_cost)
                     })
 
                 df_consumos = pd.DataFrame(data_consumos)
@@ -160,8 +107,8 @@ class Product(Base):
                                 "numero_de_documento": document.document_number,
                                 "oficina": document.office,
                                 "cantidad": document_detail.quantity,
-                                "valor_unitario": document_detail.net_total_value,
-                                "valor_total": document_detail.net_total_value * document_detail.quantity
+                                "valor_unitario": format_number(document_detail.net_total_value),
+                                "valor_total": format_number(document_detail.net_total_value * document_detail.quantity)
                             })
 
                 df_ventas = pd.DataFrame(data_ventas)
@@ -173,14 +120,6 @@ class Product(Base):
                 product_data = {
                     **product.__dict__,
                     "stock": stock,
-                    "reception": reception,
-                    "reception_details": reception_details,
-                    "consumptions": consumptions,
-                    "consumption_details": consumption_details,
-                    "documents": documentos,
-                    "document_details": document_details,
-                    "sales": sales,
-                    "sales_docs": sales_docs,
                     "reception_details_list": reception_details_list,
                     "consumption_details_list": consumption_details_list,
                     "sales_list": sales_list
