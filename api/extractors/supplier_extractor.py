536e9063b843590d2c4f7e69f6d682dc2e6f0f3a
Franco Anfossi <franco.anfossi@uc.cl>
2024-01-12 20:52:49 -0300

    supplier listo

--- api/extractors/supplier_extractor.py
+++ api/extractors/supplier_extractor.py
@@ -2,7 +2,7 @@ import requests
 import json
 import pandas as pd
 
-SUPPLIERS = "https://gist.githubusercontent.com/franco-anfossi/06a273959c67287a879980dba725b248/raw/9d7d64208e770a725b36ce3cdf709dc934d75632/gistfile1.txt"
+SUPPLIERS = "https://gist.githubusercontent.com/franco-anfossi/06a273959c67287a879980dba725b248/raw/d04ad1c9bc6178315342d1677a4e3232933e7953/gistfile1.txt"
 
 response = requests.get(SUPPLIERS)
 
