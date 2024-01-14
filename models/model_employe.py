4293d2a768256240048fa84fdc8d76e4884eb75e
mtombolini <mtombolini@uc.cl>
2024-01-13 22:35:47 -0300

    Actualiacion rrhh

--- models/model_employe.py
+++ models/model_employe.py
@@ -4,22 +4,17 @@ from databases.session import AppSession
 class ModelEmployee:
 
     @classmethod
-    def create(cls, username, rut, nombre, apellido, fecha_incorporacion, horario_colacion):
+    def create(cls, user_id, rut, nombre, apellido, fecha_incorporacion, horario_colacion):
         session = AppSession()
         try:
-            # Crea un nuevo empleado
-            new_employee = Employee(username=username, rut=rut, nombre=nombre, apellido=apellido, fecha_incorporacion=fecha_incorporacion, horario_colacion=horario_colacion)
-            
-            # Agrega el nuevo empleado a la sesión de SQLAlchemy y realiza la inserción en la base de datos
+            new_employee = Employee(user_id=user_id, rut=rut, nombre=nombre, apellido=apellido, fecha_incorporacion=fecha_incorporacion, horario_colacion=horario_colacion)
             session.add(new_employee)
             session.commit()
-            
-            print(f"Empleado creado: Username={new_employee.username}, Rut={new_employee.rut}, Nombre={new_employee.nombre}, Apellido={new_employee.apellido}")
-
             return new_employee
         except Exception as ex:
             session.rollback()
             raise ex
+
         
     @classmethod
     def get_all_employees(cls):
