b90776ecbecbaec30c51e1b1cf26377ac5c86574
DIEGO NAVARRO <dgnavarro@uc.cl>
2024-01-15 18:23:55 -0300

    Set backend Employee model

---
+++ models/employee.py
@@ -0,0 +1,146 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from databases.base import Base
+from databases.session import AppSession
+from typing import List, Dict
+
+class EmployeeNotFoundError(ValueError):
+    """Exception raised when an employee is not found."""
+    
+class EmployeeAttributeNotFoundError(AttributeError):
+    """Exception raised when trying to access an inexistent employee attribute."""
+
+class Employee(Base):
+    __tablename__ = 'empleados'
+
+    id = Column(Integer, primary_key=True)
+    # Establecer una relación con el ID del usuario en la tabla 'usuarios'
+    user_id = Column(Integer, ForeignKey('usuarios.id'))
+    # Crear la relación con el modelo User, utilizando 'user' como el nombre de la relación
+    user = relationship('User', back_populates='empleados')
+    rut = Column(String(255))
+    nombre = Column(String(255))
+    apellido = Column(String(255))
+    fecha_incorporacion = Column(String(255))
+    horario_colacion = Column(String(255))  # Nombre de variable ajustado
+
+    @classmethod
+    def get_all(cls) -> List[Dict]:
+        with AppSession() as session:
+            try:
+                employees: List[cls] = session.query(cls).all()
+                
+                # Iterate through all attributes of each Employee object
+                employees_data = [
+                    {
+                        key: value 
+                        for key, value 
+                        in employee.__dict__.items() 
+                        if not key.startswith('_')
+                    }
+                    for employee in employees
+                ]
+                return employees_data
+            except Exception as ex:
+                # Undo any changes made to session
+                raise
+            
+            
+    @classmethod
+    def get(cls, employee_id: int) -> Dict:
+        with AppSession() as session:
+            try:
+                employee = session.query(cls).filter(cls.id == employee_id).one_or_none()
+                if employee:
+                    employee_data = {
+                        key: value 
+                        for key, value 
+                        in employee.__dict__.items() 
+                        if not key.startswith('_')
+                    }
+                    return employee_data
+                else:
+                    raise EmployeeNotFoundError(f'employee with ID {employee_id} not found.')
+            except Exception as ex:
+                raise
+            
+            
+    @classmethod
+    def create(cls, **kwargs) -> Dict:
+        # Start a new session
+        with AppSession() as session:
+            try:
+                # Create an instance of the class with the given kwargs
+                new_employee = cls()
+                # Iterate over the kwargs and set them on the instance if they are valid attributes
+                for key, value in kwargs.items():
+                    if hasattr(new_employee, key):
+                        setattr(new_employee, key, value)
+                    else:
+                        # Raise an exception if we try to edit an inexistent attribute
+                        raise EmployeeAttributeNotFoundError(
+                            f'Attribute {key} not found in employee'
+                        )
+                # Add the instance to the session
+                session.add(new_employee)
+                # Commit the changes
+                session.commit()
+                new_employee_data = {
+                    key: value 
+                    for key, value 
+                    in kwargs.items()
+                }
+                return new_employee_data
+            except Exception as ex:
+                # Undo any changes made to session
+                session.rollback()
+                raise
+            
+            
+    @classmethod
+    def edit(cls, employee_id, **kwargs) -> Dict:
+        # Start a new session
+        with AppSession() as session:
+            try:
+                # Find the employee object to edit
+                employee_to_edit = session.query(cls).filter(cls.id == employee_id).one_or_none()
+                # Iterate over the kwargs and reset the values with them if they are valid attributes
+                for key, value in kwargs.items():
+                    if hasattr(employee_to_edit, key):
+                        setattr(employee_to_edit, key, value)
+                    else:
+                        # Raise an exception if we try to edit an inexistent attribute
+                        raise EmployeeAttributeNotFoundError(
+                            f'Attribute {key} not found in employee'
+                        )
+                # Commit the changes
+                session.commit()
+                edited_employee_data = {
+                    key: value 
+                    for key, value 
+                    in kwargs.items()
+                }
+                return edited_employee_data
+            except Exception as ex:
+                # Undo any changes made to session
+                session.rollback()
+                raise
+            
+            
+    @classmethod
+    def delete(cls, employee_id: int) -> None:
+        with AppSession() as session:
+            try:
+                # Find the employee object to delete
+                employee_to_delete = session.query(cls).filter(cls.id == employee_id).one_or_none()
+                # If it exists, delete it
+                # Return bool indicating if the operation was successful
+                if employee_to_delete:
+                    session.delete(employee_to_delete)
+                    session.commit()
+                else:
+                    raise EmployeeNotFoundError(f'employee with ID {employee_id} not found.')
+            except Exception as ex:
+                # Undo any changes made to session
+                session.rollback()
+                raise           
