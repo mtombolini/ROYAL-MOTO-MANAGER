5f478e422fa9e34b100859cf657ffa7407f0aa0b
Franco Anfossi <franco.anfossi@uc.cl>
2024-02-13 11:04:28 -0300

    add: office model & extractor

---
+++ models/office.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String
+from databases.base import Base
+from databases.session import AppSession
+
+class Office(Base):
+    __tablename__ = 'officinas'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    address = Column(String(255))
+    municipality = Column(String(255))
+    city = Column(String(255))
+    country = Column(String(255))
+    active_state = Column(String(255))
+    latitude = Column(String(255))
+    longitude = Column(String(255))
+
+    @classmethod
+    def get_all_offices(cls):
+        with AppSession() as session:
+            try:
+                offices = session.query(cls).all()
+                return offices
+            except Exception as ex:
+                raise
