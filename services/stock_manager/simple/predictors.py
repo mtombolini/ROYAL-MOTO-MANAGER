fix: Use floor() instead of ceil() to compute units to buy.

services/stock_manager/simple/predictors.py
@@ -1,7 +1,7 @@
 import warnings
 import pandas as pd
 
-from math import ceil
+from math import ceil, floor
 from scipy.stats import norm
 
 from services.stock_manager.parameters_service import CERTAINTY, DAYS_OF_ANTICIPATION, DAYS_TO_LAST, ONE_DATA_POINT_PONDERATOR
@@ -49,17 +49,17 @@ def units_to_buy(product_data: pd.DataFrame, days_to_last: int) -> Dict[str, int
 
     if mean * std == 0 and days_considered <= 6:
         return {
-            'without_confidence': max(0, ceil((historic_mean * days_to_last - product_data.iloc[-1]["Close"]) * ONE_DATA_POINT_PONDERATOR)),
+            'without_confidence': max(0, floor((historic_mean * days_to_last - product_data.iloc[-1]["Close"]) * ONE_DATA_POINT_PONDERATOR)),
         }
     elif mean * std == 0:
         return {
-            'without_confidence': max(0, ceil(historic_mean * days_to_last - product_data.iloc[-1]["Close"])),
+            'without_confidence': max(0, floor(historic_mean * days_to_last - product_data.iloc[-1]["Close"])),
         }
     return {
-        'without_confidence': max(0, ceil(mean * days_to_last - product_data.iloc[-1]["Close"])),
+        'without_confidence': max(0, floor(mean * days_to_last - product_data.iloc[-1]["Close"])),
         'with_confidence': np.array([
             max(0, int(lower_bound_ci * days_to_last - product_data.iloc[-1]["Close"])), 
-            ceil(upper_bound_ci * days_to_last - product_data.iloc[-1]["Close"]),
+            floor(upper_bound_ci * days_to_last - product_data.iloc[-1]["Close"]),
         ]),
     }
 