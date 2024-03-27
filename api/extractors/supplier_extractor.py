e4f6d99e4f49f339262b845d7de627fde0a46dfa
franco <franco.anfossi@uc.cl>
2024-03-27 07:42:51 -0300

    change: D mayus suppliers url

--- api/extractors/supplier_extractor.py
+++ api/extractors/supplier_extractor.py
@@ -2,7 +2,7 @@ import requests
 import json
 import pandas as pd
 
-SUPPLIERS = "https://gist.githubusercontent.com/franco-anfossi/fe52edd6684e3bed679ba68ca4850fe8/raw/8ff53edf6cf44c27ca9d1f8d0b352f1041fd6e00/suppliers"
+SUPPLIERS = "https://gist.githubusercontent.com/franco-anfossi/fe52edd6684e3bed679ba68ca4850fe8/raw/f94c3fab7120783567c1cfe68837b9e67d9c8329/suppliers"
 
 CONNECTIONS = "https://gist.githubusercontent.com/franco-anfossi/4bea704c796a6a2a36429b56b9ad3767/raw/109f23cb65b2a4add7582c8af374e0d9c95c29b9/conections-suppliers"
 
