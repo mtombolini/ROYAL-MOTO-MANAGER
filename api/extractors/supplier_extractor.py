691e29922b1fbf5e02d1fc369e92394fce3fe629
franco <franco.anfossi@uc.cl>
2024-03-27 01:46:41 -0300

    change: supplier gist url change

--- api/extractors/supplier_extractor.py
+++ api/extractors/supplier_extractor.py
@@ -2,7 +2,7 @@ import requests
 import json
 import pandas as pd
 
-SUPPLIERS = "https://gist.githubusercontent.com/franco-anfossi/fe52edd6684e3bed679ba68ca4850fe8/raw/be810c3c0fa90ed47b5821042cd4c14204da3aa5/suppliers"
+SUPPLIERS = "https://gist.githubusercontent.com/franco-anfossi/fe52edd6684e3bed679ba68ca4850fe8/raw/8ff53edf6cf44c27ca9d1f8d0b352f1041fd6e00/suppliers"
 
 CONNECTIONS = "https://gist.githubusercontent.com/franco-anfossi/4bea704c796a6a2a36429b56b9ad3767/raw/109f23cb65b2a4add7582c8af374e0d9c95c29b9/conections-suppliers"
 
